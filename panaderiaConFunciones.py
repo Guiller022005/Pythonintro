bb20ab012aaf6a112d972cbecbcb2dd4b856c382
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-02-29 14:54:37 -0500

    feat: :construction: Se añadio bienvenida al usuario
    Se imprime Bienvenida en pantalla

--- panaderiaConFunciones.py
+++ panaderiaConFunciones.py
@@ -23,58 +23,64 @@ menu = dict({
     }
 })
 
+# Bienvenida al usuario
+print("Bienvenido Punto dulce JM")
+print()
 print("Seleccione la categoria")
+
 listaCategoria = menu.keys()
 listaCategoria = list(listaCategoria)
 for i,val in enumerate(listaCategoria):
     print(f"{i}.{val}")
 opcionCategoria = int(input())
-datosCategoria = menu.get(listaCategoria[opcionCategoria])
+datosCategoria = menu.get(listaCategoria[opcionCategoria ])
 productosCategoria = datosCategoria["Producto"]
 
-print(f"Seleccione el producto de la categoria {listaCategoria[opcionCategoria]}")
-for i, val in enumerate(productosCategoria):
+# Menu del usuario
+print("Menu de seleccion de productos")
+print(f"Seleccione el producto de la categoria {listaCategoria[opcionCategoria ]}")
+for i, val in enumerate(productosCategoria ):
     print(f"{i}.{val}")
 opcionProducto = int(input())
 
 
 # Modulo para buscar si el producto tiene promociones segun la categoria del seleccionado
-datosCategoria = menu.get(listaCategoria[opcionCategoria])
+datosCategoria = menu.get(listaCategoria[opcionCategoria ])
 promocionesProducto = datosCategoria.get("Promociones")
 promocionProductos = list()
 for val in promocionesProducto:
-    if(val.get("indice") == opcionProducto):
+    if(val.get("indice") == opcionProducto ):
         promocionProductos.append(val)
 
 
 # Modulo para saber la lista de promociones que tiene un producto seleccionado segun la categoria
 if(len(promocionProductos) == 0):
-    producto = datosCategoria.get('Producto')[opcionProducto]
+    producto = datosCategoria.get('Producto')[opcionProducto ]
     nombreProducto = producto.get("nombre")
     productoValor = producto.get("valor")
     cantidad = int(input(f"Usuario cuantos {producto} desea: "))
     valorAPagar = cantidad * productoValor
-    print(f"Usuario su producto {nombreProducto} tiene un valor de ${productoValor} la cantidad solicitada es de {cantidad} que da un total a pagar de ${valorAPagar} ")
+    print(f"\nUsuario su producto {nombreProducto} tiene un valor de ${productoValor} la cantidad solicitada es de {cantidad} que da un total a pagar de ${valorAPagar} ")
     dinero = int(input("ingrese la cantidad de dinero disponible: "))
     if(dinero >= valorAPagar):
         vueltos = dinero - valorAPagar
-        print(f"Usuario sus vueltos son {vueltos}, gracias por su compra")
+        print(f"\nUsuario sus vueltos son {vueltos}\n, gracias por su compra")
     else: 
         print(f"Usuario el monto es menor al valor total, porfavor busque dinero")
 else:
-    print(f"Seleccione una promocion del producto {datosCategoria.get('Producto')[opcionProducto]}")
+    print(f"Seleccione una promocion del producto {datosCategoria.get('Producto')[opcionProducto ]}")
     for i, val in enumerate(promocionProductos):
         print(f"{i}.{val}")
     print(f"{len(promocionProductos)}. Salir")
     opcionPromocion = int(input())
     if(opcionPromocion !=  2):
-        producto = datosCategoria.get('Producto')[opcionProducto]
+        producto = datosCategoria.get('Producto')[opcionProducto ]
         nombreProducto = producto.get("nombre")
         productoValor = producto.get("valor")
 
-        nombrePromocion = promocionProductos[opcionPromocion].get("nombre")
-        descuentoPromocion = promocionProductos[opcionPromocion].get("descuento")
-        unidadesPromocion = promocionProductos[opcionPromocion].get("unidades")
+        nombrePromocion = promocionProductos[opcionPromocion ].get("nombre")
+        descuentoPromocion = promocionProductos[opcionPromocion ].get("descuento")
+        unidadesPromocion = promocionProductos[opcionPromocion ].get("unidades")
 
 
         valorAPagar = productoValor - ((unidadesPromocion * productoValor)* descuentoPromocion)
@@ -82,12 +88,12 @@ else:
         dinero = int(input("Ingrese la cantidad de dinero disponible "))
         if(dinero >= valorAPagar):
             vueltos = dinero - valorAPagar
-            print(f"El valor a pagar es de ${valorAPagar} de la promocion {promocionProductos[opcionPromocion]}")
+            print(f"El valor a pagar es de ${valorAPagar} de la promocion {promocionProductos[opcionPromocion ]}")
             print(f"Usuario sus vueltos son {vueltos}, Gracias por comprar nuestro producto")
         else:
             print(f"Usuario el monto es menor a valor total, porfavor busque plata")
     else:
-        producto = datosCategoria.get('Producto')[opcionProducto]
+        producto = datosCategoria.get('Producto')[opcionProducto -1]
         nombreProducto = producto.get("nombre")
         productoValor = producto.get("valor")
         cantidad = int(input(f"Usuario cuantos {producto} desea: "))
@@ -96,7 +102,7 @@ else:
         dinero = int(input("Ingrese la cantidad de dinero disponible: "))
         if(dinero >= valorAPagar):
             vueltos = dinero - valorAPagar
-            print(f"Usuario sus vueltos son {vueltos}, gracias por su compra")
+            print(f"\nUsuario sus vueltos son {vueltos}\n, gracias por su compra")
         else:
-            print("Usuario el monto es menor al valor total, porfavor busque mas dinero")
+            print("\nUsuario el monto es menor al valor total, porfavor busque mas dinero")
 
